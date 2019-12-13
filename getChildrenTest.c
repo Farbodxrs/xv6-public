9a9a4451a57474e93521780b90c73672566d4afa
farbodrasaei <farbod.rasaei@snapp.cab>
2019-12-13 20:47:03 +0330

    nothing

--- getChildrenTest.c
+++ getChildrenTest.c
@@ -3,20 +3,9 @@
 
 int main(void) {
 
-    int child = fork();
-    if(child < 0){
-        printf(1, "fork error");
-        exit();
-    }
-    if (child == 0) {
-        //parent
-        wait();
-        printf(1, "process(child) : pid()= %d ppid()= %d getChildren(getppid())= %d\n", getpid(), getppid(), getchildren(getppid()));
-    } if(child > 0){
-        //child
-
-        printf(1, "process(parent) : pid()=%d ppid()=%d getChildren(getppid())=%d\n",
-               getpid(), getppid(), getchildren(getppid()));
-    }
+    fork();
+    fork();
+    printf(1, "process : pid()= %d ppid()= %d getChildren(getppid())= %d\n", getpid(), getppid(),getchildren(getppid()));
+    wait();
     exit();
 }
