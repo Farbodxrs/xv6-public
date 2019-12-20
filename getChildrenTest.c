3d35c8189bc77679d4a037369bf2483d4b341bdd
farbodrasaei <farbod.rasaei@snapp.cab>
2019-12-20 17:28:45 +0330

    fix get children zombie

--- getChildrenTest.c
+++ getChildrenTest.c
@@ -3,10 +3,18 @@
 
 int main(void) {
 
-    fork();
-    fork();
-    wait();
+    int a = fork();
+    int b = fork();
+    if (a > 0) {
+        //parent
+        sleep(30);
+    }
+
+    if (b > 0) {
+        sleep(20);
+    }
     printf(1, "process : pid()= %d ppid()= %d getChildren(getppid())= %d\n", getpid(), getppid(),getchildren(getppid()));
+    wait();
     wait();
     exit();
 }
